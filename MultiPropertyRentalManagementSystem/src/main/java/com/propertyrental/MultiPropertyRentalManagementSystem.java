d2240c92841141f053def9885898acbf0456634f
Hossam Metwalli <57620818+Hossammetwalli@users.noreply.github.com>
2021-02-18 17:50:17 +0200

    MultiPropertyRentalManagement

---
+++ MultiPropertyRentalManagementSystem/src/main/java/com/propertyrental/MultiPropertyRentalManagementSystem.java
@@ -0,0 +1,24 @@
+package com.propertyrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef="auditorAware")
+public class MultiPropertyRentalManagementSystem {
+	
+	
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return new SpringSecurityAuditorAware();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPropertyRentalManagementSystem.class, args);
+	}
+
+}
---
+++ MultiPropertyRentalManagementSystem/src/test/java/com/propertyrental/MultiPropertyRentalManagementSystemTests.java
@@ -0,0 +1,13 @@
+package com.propertyrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPropertyRentalManagementSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
